26ee3789fa1fd9238a16b65d0a8cf129eaf61a79
Volodymyr Kopynets <vovakopinecz@gmail.com>
2023-08-03 14:44:10 +0300

    Update register page with new register logic

--- src/app/features/registerScheme.ts
+++ src/app/features/registerScheme.ts
@@ -1,17 +1,32 @@
 import { z } from 'zod';
+import { NewUser_T } from '@/types/models.ts';
 
 export const RegisterValidator = z.object({
   email: z
     .string()
-    .email({ message: 'Email field must be required as an email' }),
+    .nonempty({ message: 'Email is not allowed to be empty' })
+    .email({ message: 'Email field must be a valid email' }),
   password: z
     .string()
-    .min(8, { message: 'Password must be at least 8 characters long' }),
+    .min(5, { message: 'Password must be at least 5 characters long' })
+    .max(10, {
+      message: 'Password must be at less or equal to 10 characters long',
+    }),
   name: z.string(),
   extra_details: z.string(),
   skills: z.string(),
   profession: z.string(),
   details: z.string(),
 });
+
+export const defaultRegisterValues: NewUser_T = {
+  details: '',
+  email: '',
+  extra_details: '',
+  name: '',
+  skills: '',
+  password: '',
+  profession: '',
+};
 
 export type RegisterCredentials = z.infer<typeof RegisterValidator>;
